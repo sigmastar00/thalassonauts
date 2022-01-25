2d17c5c8ecdf397d74213e44dd02afdd87d17dd3
SigmaStar <sigmastar00@gmail.com>
2022-01-25 12:58:26 -0800

    Remove mac version

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
-godot --export HTML5.release export/HTML5.release/index.html
-butler push export/HTML5.release/ sigmastar/gwj41:html
+godot --no-window --export HTML5.release export/HTML5.release/index.html 
+godot --no-window --export Linux.release export/Linux.release/thalassonauts.x86_64 
+godot --no-window --export Windows.release export/Windows.release/thalassonauts.exe 
+butler push export/HTML5.release/ sigmastar/thalassonauts:html
+butler push export/Linux.release/ sigmastar/thalassonauts:linux
+butler push export/Windows.release/ sigmastar/thalassonauts:windows
+
 
